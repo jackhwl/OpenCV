9d23cc0c02421d030b6827fc2cca4d29b639aa67
Wenlin Huang <jackhwl@hotmail.com>
2022-03-01 21:10:43 -0500

    BGR vs RGB

--- ex/matplotlib_display.py
+++ ex/matplotlib_display.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-
-image = mpimg.imread("../images/chelsea-the-cat.jpeg")
+import cv2
+image = cv2.imread("../images/chelsea-the-cat.jpeg")
 plt.axis("off")
-plt.imshow(image)
+plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 plt.show()
