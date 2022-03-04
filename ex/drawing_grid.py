46450dbd34b2bbf405f638451850003bb8e08b69
Wenlin Huang <jackhwl@hotmail.com>
2022-03-04 18:36:28 -0500

    drawing grid v2

--- ex/drawing_grid.py
+++ ex/drawing_grid.py
@@ -4,14 +4,15 @@ import cv2
 canvas = np.zeros((300, 300, 3), dtype = "uint8")
 
 red = (0, 0, 255)
-lag = 100
+lag = 50
 
 for row in range(0, 300 // lag):
-    for col in range(0, 300 // lag - 1):
+    for col in range(0, (300 + lag) // (2 * lag) ):
         x = (lag if row % 2 == 0 else 0) + col * lag * 2
         y = row * lag
         print(x, y, x+lag, y+lag) 
-        cv2.rectangle(canvas, (x, y), (x+lag, y+lag), red, -1)
+        if x < 300 and y < 300:
+            cv2.rectangle(canvas, (x, y), (x+lag, y+lag), red, -1)
 
 (centerX, centerY) = (canvas.shape[1] // 2, canvas.shape[0] // 2)
 green = (0, 255, 0)
