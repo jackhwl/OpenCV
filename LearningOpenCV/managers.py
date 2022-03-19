55399b8e749ab554954ac874827c8d847ddc87ae
Wenlin Huang <jackhwl@hotmail.com>
2022-03-18 20:08:47 -0400

    managers

--- LearningOpenCV/managers.py
+++ LearningOpenCV/managers.py
@@ -3,6 +3,7 @@ import numpy
 import time
 
 class CaptureManager(object):
+
     def __init__(self, capture, previewWindowManager = None, shouldMirrorPreview = False):
         self.previewWindowManager = previewWindowManager
         self.shouldMirrorPreview = shouldMirrorPreview
@@ -125,3 +126,32 @@ class CaptureManager(object):
 
         self._videoWriter.write(self._frame)
                 
+class WindowManager(object):
+
+    def __init__(self, windowName, keypressCallback = None):
+        self.keypressCallback = keypressCallback
+
+        self._windowName = windowName
+        self._isWindowCreated = False
+
+    @property
+    def isWindowCreated(self):
+        return self._isWindowCreated
+
+    def createWindow (self):
+        cv2.namedWindow(self._windowName)
+        self._isWindowCreated = True
+
+    def show(self, frame):
+        cv2.imshow(self._windowName, frame)
+
+    def destroyWindow (self):
+        cv2.destroyWindow(self._windowName)
+        self._isWindowCreated = False
+
+    def processEvents (self):
+        keycode = cv2.waitKey(1)
+        if self.keypressCallback is not None and keycode != -1:
+            # Discard any non-ASCII info encoded by GTK.
+            keycode &= 0xFF
+            self.keypressCallback(keycode)
