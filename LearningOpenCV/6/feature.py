6b120f8f0555970498e5624620d64bfe908f6c6c
Wenlin Huang <jackhwl@hotmail.com>
2022-04-02 23:04:57 -0400

    SIFT replace flag enum with number

--- LearningOpenCV/6/feature.py
+++ LearningOpenCV/6/feature.py
@@ -11,10 +11,11 @@ sift = cv2.SIFT_create() #cv2.xfeatures2d.SIFT_create()
 keypoints, descriptor = sift.detectAndCompute(gray,None)
 
 #img = cv2.drawKeypoints(image=img, outImage=img, keypoints = keypoints, flags = cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINT, color = (51, 163, 236))
+img2 = cv2.drawKeypoints(image=img, outImage=img, keypoints = keypoints, flags = 4, color = (51, 163, 236))
 
-img2 = img.copy()
-for marker in keypoints:
-	img2 = cv2.drawMarker(img2, tuple(int(i) for i in marker.pt), color=(51, 163, 236))
+#img2 = img.copy()
+#for marker in keypoints:
+#	img2 = cv2.drawMarker(img2, tuple(int(i) for i in marker.pt), color=(51, 163, 236))
 
 cv2.imshow('sift_keypoints', img2)
 while (True):
