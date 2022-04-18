9250a427b6a77b2dda8aa568b4ea72f112eb6726
Wenlin Huang <jackhwl@hotmail.com>
2022-04-17 21:10:33 -0400

    tensorflow intro

---
+++ MasterOpenCV_Alberto/12/tensorflow/exp1.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+
+logs_path = "./logs"
+a = 2
+b = 3
+c = tf.add(a, b, name='Add')
+
+with tf.compat.v1.Session() as sess:
+    summary_writer = tf.compat.v1.summary.FileWriter(logs_path, sess.graph)
+    print(c)
