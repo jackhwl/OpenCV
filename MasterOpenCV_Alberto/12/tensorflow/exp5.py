baa64a4a70f499b9a4d1b8264898c114e327a244
Wenlin Huang <jackhwl@hotmail.com>
2022-04-18 19:48:30 -0400

    variables

--- MasterOpenCV_Alberto/12/tensorflow/exp5.py
+++ MasterOpenCV_Alberto/12/tensorflow/exp5.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 a = tf.get_variable(name="var_1", initializer=tf.constant(2))
 b = tf.get_variable(name="var_2", initializer=tf.constant(3))
 c = tf.add(a, b, name="Add1")
+
 # add an Op to initialize global variables
 init_op = tf.global_variables_initializer()
 
