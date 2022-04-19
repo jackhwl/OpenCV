f97f1b79040c9040f51f3e0d4090e673580519d8
Wenlin Huang <jackhwl@hotmail.com>
2022-04-18 20:11:08 -0400

    placeholder

---
+++ MasterOpenCV_Alberto/12/tensorflow/exp7.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+
+# create graph
+a = tf.constant([5, 5, 5], tf.float32, name='A')
+b = tf.placeholder(tf.float32, shape=[3], name='B')
+c = tf.add(a, b, name="Add")
+
+with tf.Session() as sess:
+# create a dictionary:
+    d = {b: [1, 2, 3]}
+    # feed it to the placeholder
+    print(sess.run(c, feed_dict=d)) 
+ 
