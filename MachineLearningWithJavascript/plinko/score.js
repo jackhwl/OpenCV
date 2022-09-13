d30fe0c539e658dd0f71503b1ae6bf3f37d93955
Wenlin Huang <whuang@viglobal.com>
2022-09-13 10:12:13 -0400

    32. Applying Normalization

--- MachineLearningWithJavascript/plinko/score.js
+++ MachineLearningWithJavascript/plinko/score.js
@@ -6,7 +6,7 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 
 function runAnalysis() {
     const testSetSize = 10
-    const [testSet, trainingSet] = splitDataset(outputs, testSetSize)
+    const [testSet, trainingSet] = splitDataset(minMax(outputs, 3), testSetSize)
     // let numberCorrect = 0
     // for (let i=0; i < testSet.length; i++) {
     //     const bucket = knn(trainingSet, testSet[i][0])
