ace0c19780e0a4bb9fc583066d856a1664069c18
Wenlin Huang <whuang@viglobal.com>
2022-10-14 09:39:04 -0400

    78. Initial Gradient Descent Implementation

--- MachineLearningWithJavascript/regressions/linear-regression.js
+++ MachineLearningWithJavascript/regressions/linear-regression.js
@@ -6,6 +6,13 @@ class LinearRegression {
         this.labels = labels
 
         this.options = Object.assign({ learningRate: 0.1, iterations: 1000 }, options)
+
+        this.m = 0
+        this.b = 0
+    }
+
+    gradientDescent() {
+        const currentGuessesForMPG = this.features.map(row => this.m * row[0] + this.b)
     }
 
     train() {
