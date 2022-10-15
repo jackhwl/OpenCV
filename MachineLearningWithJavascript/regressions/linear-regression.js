75a5a6336a3e23957c3eb7788907ccb7c27560dc
Wenlin Huang <whuang@viglobal.com>
2022-10-15 14:52:51 -0400

    79. Calculating MSE Slopes

--- MachineLearningWithJavascript/regressions/linear-regression.js
+++ MachineLearningWithJavascript/regressions/linear-regression.js
@@ -1,5 +1,5 @@
 const tf = require('@tensorflow/tfjs')
-
+const _ = require('lodash')
 class LinearRegression {
     constructor(features, labels, options) {
         this.features = features
@@ -13,6 +13,8 @@ class LinearRegression {
 
     gradientDescent() {
         const currentGuessesForMPG = this.features.map(row => this.m * row[0] + this.b)
+        const bSlope = _.sum(currentGuessesForMPG.map((guess, i) => guess - this.labels[i][0])) * 2 / this.features.length
+        const mSlope = _.sum(currentGuessesForMPG.map((guess, i) => -1 * this.features[i][0] * (this.labels[i][0] - guess))) * 2 / this.features.length
     }
 
     train() {
