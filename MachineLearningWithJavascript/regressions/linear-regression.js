2e4b1827509140200c753028bcd81bba1948d110
Wenlin Huang <whuang@viglobal.com>
2022-10-24 18:23:45 -0400

    88. Refactoring to One Equation

--- MachineLearningWithJavascript/regressions/linear-regression.js
+++ MachineLearningWithJavascript/regressions/linear-regression.js
@@ -8,11 +8,15 @@ class LinearRegression {
         this.features = tf.ones([this.features.shape[0], 1]).concat(this.features, 1)
         this.options = Object.assign({ learningRate: 0.1, iterations: 1000 }, options)
 
-        this.m = 0
-        this.b = 0
+        this.weights = tf.zeros([2, 1])
     }
-
     gradientDescent() {
+        const currentGuesses = this.features.matMul(this.weights)
+        const differences = currentGuesses.sub(this.labels)
+
+        const slopes = this.features.transpose().matMul(differences).div(this.features.shape[0])
+    }
+    gradientDescent0() {
         const currentGuessesForMPG = this.features.map(row => this.m * row[0] + this.b)
         const bSlope = _.sum(currentGuessesForMPG.map((guess, i) => guess - this.labels[i][0])) * 2 / this.features.length
         const mSlope = _.sum(currentGuessesForMPG.map((guess, i) => -1 * this.features[i][0] * (this.labels[i][0] - guess))) * 2 / this.features.length
