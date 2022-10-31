042734bb7e87d3dd2bdd06bd736facd0760957ec
Wenlin Huang <whuang@viglobal.com>
2022-10-30 20:50:55 -0400

    105. Plotting MSE Values

--- MachineLearningWithJavascript/regressions/index.js
+++ MachineLearningWithJavascript/regressions/index.js
@@ -2,6 +2,7 @@ require('@tensorflow/tfjs-node')
 const tf = require('@tensorflow/tfjs')
 const loadCSV = require('./load-csv')
 const LinearRegression = require('./linear-regression')
+const plot = require('node-remote-plot')
 
 let { features, labels, testFeatures, testLabels } = loadCSV('./data/cars.csv', {
     shuffle: true,
@@ -11,13 +12,19 @@ let { features, labels, testFeatures, testLabels } = loadCSV('./data/cars.csv', 
 })
 
 const regression = new LinearRegression(features, labels, {
-    learningRate: 10,
+    learningRate: .1,
     iterations: 100
 })
 
 regression.train()
 //regression.features.print()
 const r2 = regression.test(testFeatures, testLabels)
-console.log('MSE History', regression.mseHistory)
+
+plot({
+    x: regression.mseHistory.reverse(),
+    xLabel: 'Iteration #',
+    yLabel: 'Mean Squared Error'
+})
+
 console.log('r2=', r2)
 //console.log('Updated M is:', regression.weights.get(1, 0), 'Updated B is:', regression.weights.get(0, 0))
