d4a55f2e965ab4b1eac1684b72d0aed9cea993eb
Wenlin Huang <whuang@viglobal.com>
2022-11-04 15:31:35 -0400

    119. Project Setup for Logistic Regression

---
+++ MachineLearningWithJavascript/regressions/logistic/index.js
@@ -0,0 +1,35 @@
+require('@tensorflow/tfjs-node')
+const tf = require('@tensorflow/tfjs')
+const loadCSV = require('../load-csv')
+const LinearRegression = require('./logistic-regression')
+const plot = require('node-remote-plot')
+
+let { features, labels, testFeatures, testLabels } = loadCSV('../data/cars.csv', {
+    shuffle: true,
+    splitTest: 50,
+    dataColumns: ['horsepower', 'weight', 'displacement'],
+    labelColumns: ['mpg']
+})
+
+const regression = new LinearRegression(features, labels, {
+    learningRate: .1,
+    iterations: 3,
+    batchSize: 10
+})
+
+regression.train()
+//regression.features.print()
+const r2 = regression.test(testFeatures, testLabels)
+
+plot({
+    x: regression.mseHistory.reverse(),
+    xLabel: 'Iteration #',
+    yLabel: 'Mean Squared Error'
+})
+
+console.log('r2=', r2)
+
+regression.predict([
+    [120, 2, 380]
+]).print()
+//console.log('Updated M is:', regression.weights.get(1, 0), 'Updated B is:', regression.weights.get(0, 0))
