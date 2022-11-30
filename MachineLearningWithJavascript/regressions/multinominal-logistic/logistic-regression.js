d742187db3343998628f7a63a6aeba1fab2adb61
Wenlin Huang <whuang@viglobal.com>
2022-11-29 21:04:12 -0500

    164. Optimization Tensorflow Memory Usage

--- MachineLearningWithJavascript/regressions/multinominal-logistic/logistic-regression.js
+++ MachineLearningWithJavascript/regressions/multinominal-logistic/logistic-regression.js
@@ -40,7 +40,6 @@ class LogisticRegression {
                 const labelSlice = this.labels.slice([startIndex, 0], [batchSize, -1])
                 this.gradientDescent(featureSlice, labelSlice)
             }
-            debugger
             this.recordCost()
             this.updateLearningRate()
         }
